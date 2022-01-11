e9b12ff2ba3334c72808a11e4ab75dedcdf4815a
Shunzhe <git@mszmagic.com>
2022-01-11 23:22:34 +0800

    Initial Commit

---
+++ SwiftUI-Toggle-DarkMode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Toggle-DarkMode
+//
+//  Created by Shunzhe on 2022/01/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Toggle-DarkMode/SwiftUI_Toggle_DarkModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Toggle_DarkModeApp.swift
+//  SwiftUI-Toggle-DarkMode
+//
+//  Created by Shunzhe on 2022/01/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Toggle_DarkModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
